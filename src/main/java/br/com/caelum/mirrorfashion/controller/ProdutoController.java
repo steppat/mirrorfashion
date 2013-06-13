43a2518c54080eac02bffd58fc2d4cfbed64a713
Rômulo Henrique <romulo.henriquemv@caelum.com.br>
2013-06-13 16:14:49 -0300

    Mudando para jsonp

--- src/main/java/br/com/caelum/mirrorfashion/controller/ProdutoController.java
+++ src/main/java/br/com/caelum/mirrorfashion/controller/ProdutoController.java
@@ -1,6 +1,6 @@
 package br.com.caelum.mirrorfashion.controller;
 
-import static br.com.caelum.vraptor.view.Results.json;
+import static br.com.caelum.vraptor.view.Results.jsonp;
 
 import java.util.List;
 
@@ -9,18 +9,26 @@ import br.com.caelum.mirrorfashion.modelo.Produto;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.ioc.RequestScoped;
+import br.com.caelum.vraptor.serialization.JSONPSerialization;
 
 @Resource
+@RequestScoped
 public class ProdutoController {
 
 	@Get("/produtos")
-	public void produtos(Result result) {
-		
+	public void produtos(Result result, String callback) {
+
 		GeradorProdutos geradorProdutos = new GeradorProdutos();
 
 		List<Produto> produtos = geradorProdutos.geraProdutos();
 
-		result.use(json()).from(produtos, "produtos").serialize();
+		System.out.println(callback);
+
+		JSONPSerialization serialization = result.use(jsonp());
+
+		serialization.withCallback(callback).from(produtos, "produtos")
+				.serialize();
 
 	}
 
